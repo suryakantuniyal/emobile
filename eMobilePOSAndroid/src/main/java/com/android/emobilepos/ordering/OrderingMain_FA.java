ab487e2ecb136dca564569177c629030bd3d1c6f
Guarionex padro <gpadro@e-nablercorp.com>
2016-01-20 11:32:00 -0400

    Fix crash when fastscanning with group by sku enabled.

--- eMobilePOSAndroid/src/main/java/com/android/emobilepos/ordering/OrderingMain_FA.java
+++ eMobilePOSAndroid/src/main/java/com/android/emobilepos/ordering/OrderingMain_FA.java
@@ -687,7 +687,7 @@ public class OrderingMain_FA extends BaseFragmentActivityActionBar implements Re
                                 if (foundPosition != -1) // product already
                                 // exist in list
                                 {
-                                    global.refreshParticularOrder(myPref, foundPosition, product);
+                                    global.refreshParticularOrder(OrderingMain_FA.this, foundPosition, product);
                                 } else
                                     Catalog_FR.instance.automaticAddOrder(product);// temp.automaticAddOrder(listData);
                             } else
@@ -794,7 +794,7 @@ public class OrderingMain_FA extends BaseFragmentActivityActionBar implements Re
                                     // in
                                     // list
                                     {
-                                        global.refreshParticularOrder(myPref, foundPosition, product);
+                                        global.refreshParticularOrder(OrderingMain_FA.this, foundPosition, product);
                                     } else
                                         Catalog_FR.instance.automaticAddOrder(product);// temp.automaticAddOrder(listData);
                                 } else
@@ -886,7 +886,7 @@ public class OrderingMain_FA extends BaseFragmentActivityActionBar implements Re
                         if (foundPosition != -1) // product already exist in
                         // list
                         {
-                            global.refreshParticularOrder(myPref, foundPosition, product);
+                            global.refreshParticularOrder(OrderingMain_FA.this, foundPosition, product);
                         } else
                             Catalog_FR.instance.automaticAddOrder(product);// temp.automaticAddOrder(listData);
                     } else
