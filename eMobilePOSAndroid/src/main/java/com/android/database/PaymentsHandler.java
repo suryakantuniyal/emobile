bed429ef5c25a4546112fc2f5b53e6df716517d8
Guarionex Padro <guarionexp@gmail.com>
2016-02-16 22:36:51 -0400

    Fix store & forward payments

--- eMobilePOSAndroid/src/main/java/com/android/database/PaymentsHandler.java
+++ eMobilePOSAndroid/src/main/java/com/android/database/PaymentsHandler.java
@@ -103,6 +103,10 @@ public class PaymentsHandler {
         return lastPaymentInserted;
     }
 
+    protected static void setLastPaymentInserted(Payment payment) {
+        lastPaymentInserted = payment;
+    }
+
     public final List<String> attr = Arrays.asList(pay_id, group_pay_id, cust_id, tupyx_user_id, emp_id,
             inv_id, paymethod_id, pay_check, pay_receipt, pay_amount, pay_dueamount, pay_comment, pay_timecreated,
             pay_timesync, account_id, processed, pay_issync, pay_transid, pay_refnum, pay_name, pay_addr, pay_poscode,
