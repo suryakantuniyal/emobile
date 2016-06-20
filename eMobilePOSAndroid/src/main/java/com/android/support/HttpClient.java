b3f469e2f1605602bd5e9a17b2d2c8735e2630ce
Guarionex <gpadro@e-nablercorp.com>
2016-06-20 17:17:02 -0400

    Add auto update app download from admin settings.

--- eMobilePOSAndroid/src/main/java/com/android/support/HttpClient.java
+++ eMobilePOSAndroid/src/main/java/com/android/support/HttpClient.java
@@ -1,5 +1,20 @@
 package com.android.support;
 
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+
+import com.android.emobilepos.R;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -29,14 +44,21 @@ public class HttpClient {
     JSONObject jsono;
     HttpResponse response;
     HttpEntity entity;
-//	Ciphers ciphers = new Ciphers();
+    Handler handler;
+    ProgressDialog progressDialog;
+    private DownloadFileCallBack callback;
+    public static int incr;
+    protected static final int ID = 0;
+    private static final int COPY_STARTED = 0;
+    private static final int UPDATE_PROGRESS = 1;
+    private Context context;
 
-
-    public static void downloadFile(String urlAddress, String path) {
+    public String downloadFile(String urlAddress, String path) {
         InputStream input = null;
         OutputStream output = null;
         HttpURLConnection connection = null;
         try {
+
             URL url = new URL(urlAddress);
             connection = (HttpURLConnection) url.openConnection();
             connection.connect();
@@ -69,12 +91,30 @@ public class HttpClient {
                 // publishing the progress....
 //                if (fileLength > 0) // only if total length is known
 //                    publishProgress((int) (total * 100 / fileLength));
+                if (fileLength > 0) {
+                    if (handler != null) {
+                        Message handlerMsg = handler.obtainMessage();
+                        handlerMsg.what = UPDATE_PROGRESS;
+                        handlerMsg.arg1 = (int) (total * 100 / fileLength);
+                        handler.sendMessage(handlerMsg);
+                    }
+                    incr = (int) (total * 100 / fileLength);
+                }
                 output.write(data, 0, count);
             }
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         } finally {
             try {
+                incr = 100;
+                if (handler != null) {
+                    Message handlerMsg = handler.obtainMessage();
+                    handlerMsg.what = UPDATE_PROGRESS;
+                    handlerMsg.arg1 = 100;
+                    handler.sendMessage(handlerMsg);
+                }
+
                 if (output != null)
                     output.close();
                 if (input != null)
@@ -85,6 +125,7 @@ public class HttpClient {
             if (connection != null)
                 connection.disconnect();
         }
+        return path;
     }
 
     public InputStream httpInputStreamRequest(String url) throws ClientProtocolException,
@@ -176,7 +217,7 @@ public class HttpClient {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         StringBuilder sb = new StringBuilder();
 
-        String line = null;
+        String line;
         try {
             while ((line = reader.readLine()) != null) {
                 sb.append(line + "\n");
@@ -193,5 +234,89 @@ public class HttpClient {
         return sb.toString();
     }
 
+    public void downloadFileAsync(String urlAddress, String path, DownloadFileCallBack callBack, Context context) {
+        this.callback = callBack;
+        this.context = context;
+        setHandler();
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setIndeterminate(false);
+        progressDialog.setTitle(R.string.downloadig_update);
+        progressDialog.setProgress(0);
+        progressDialog.setMax(100);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.show();
+//        mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        mBuilder = new NotificationCompat.Builder(context)
+//                .setSmallIcon(R.drawable.ic_file_download_black_18dp)
+//                .setContentTitle(context.getString(R.string.app_name))
+//                .setContentText(context.getString(R.string.downloadig_update));
+        Message handlerMsg = handler.obtainMessage();
+        handlerMsg.what = COPY_STARTED;
+        handlerMsg.arg1 = 1;
+        handler.sendMessage(handlerMsg);
+        new DownloadFileTask().execute(urlAddress, path);
+    }
+
+    private class DownloadFileTask extends AsyncTask<Object, Void, String> {
+
+        @Override
+        protected String doInBackground(Object... params) {
+            return downloadFile((String) params[0], (String) params[1]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (TextUtils.isEmpty(s))
+                callback.downloadFail();
+            else
+                callback.downloadCompleted(s);
+        }
+    }
+
+    public interface DownloadFileCallBack {
+        public void downloadCompleted(String path);
+
+        public void downloadFail();
+    }
+
+    private void setHandler() {
+        handler = new Handler() {
+
+            public void handleMessage(android.os.Message msg) {
+                incr = msg.arg1;
+
+                if (incr < 100) {
+                    // Sets the progress indicator to a max value, the
+                    // current completion percentage, and "determinate"
+                    // state
+                    progressDialog.setProgress(incr);
+
+//                    mBuilder.setProgress(100, incr, false);
+                    // Displays the progress bar for the first time.
+//                    mNotificationManager.notify(0, mBuilder.build());
+                    // Sleeps the thread, simulating an operation
+                    // that takes time
+
+                } else {
+                    if (incr == 999) {
+                        // When the loop is finished, updates the notification
+//                        mBuilder.setContentText("Download fail.")
+//                                // Removes the progress bar
+//                                .setProgress(0, 0, false);
+//                        mNotificationManager.notify(ID, mBuilder.build());
+                    } else {
+                        // When the loop is finished, updates the notification
+                        progressDialog.setProgress(100);
+                        progressDialog.dismiss();
+//                        mBuilder.setContentText(
+//                                "Download completed.")
+//                                // Removes the progress bar
+//                                .setProgress(0, 0, false);
+//                        mNotificationManager.notify(ID, mBuilder.build());
+                    }
+                }
+            }
+        };
+    }
 }
 
