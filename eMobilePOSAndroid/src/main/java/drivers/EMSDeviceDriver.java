dd1511d984ad276c1219dbeb5332e360c82e4336
gpadro <gpadro@e-nablercorp.com>
2016-08-11 15:10:43 -0400

    Set print to log = false

--- eMobilePOSAndroid/src/main/java/drivers/EMSDeviceDriver.java
+++ eMobilePOSAndroid/src/main/java/drivers/EMSDeviceDriver.java
@@ -83,7 +83,7 @@ import plaintext.EMSPlainTextHelper;
 
 
 public class EMSDeviceDriver {
-    public static final boolean PRINT_TO_LOG = true;
+    public static final boolean PRINT_TO_LOG = false;
     protected EMSPlainTextHelper textHandler = new EMSPlainTextHelper();
     protected double itemDiscTotal = 0;
     protected double saveAmount;
