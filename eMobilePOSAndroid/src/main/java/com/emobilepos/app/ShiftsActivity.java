e9a053d8b7cb41eec6e7db589c568a991f8feed8
gpadro <gpadro@e-nablercorp.com>
2017-02-07 15:51:10 -0400

    add new shift clerk layout activity

---
+++ eMobilePOSAndroid/src/main/java/com/emobilepos/app/ShiftsActivity.java
@@ -0,0 +1,16 @@
+package com.emobilepos.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.emobilepos.R;
+
+public class ShiftsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shifts);
+    }
+
+}
