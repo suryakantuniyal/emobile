061339a7b71feb49e8386aa1bac75cc578618d91
gpadro <gpadro@e-nablercorp.com>
2017-02-15 17:57:04 -0400

    fix setting menu selection crash

--- eMobilePOSAndroid/src/main/java/com/android/dao/EmobilePOSRealmMigration.java
+++ eMobilePOSAndroid/src/main/java/com/android/dao/EmobilePOSRealmMigration.java
@@ -2,14 +2,15 @@ package com.android.dao;
 
 import io.realm.DynamicRealm;
 import io.realm.Realm;
-import io.realm.RealmMigration;
 
 /**
  * Created by Guarionex on 4/13/2016.
  */
-public class RealMigration implements RealmMigration {
+public class EmobilePOSRealmMigration implements io.realm.RealmMigration {
     @Override
     public void migrate(DynamicRealm realm, long oldVersion, long newVersion) {
-        Realm.deleteRealm(realm.getConfiguration());
+        if (oldVersion != newVersion) {
+           realm.deleteAll();
+        }
     }
 }
