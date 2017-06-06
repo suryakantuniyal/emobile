966895ebef8fddfb493fcecca34e9e26b233a76b
gpadro <gpadro@e-nablercorp.com>
2017-06-06 14:26:57 -0400

    manage settings rotation exceptions

--- eMobilePOSAndroid/src/main/java/com/android/emobilepos/settings/BixolonFragment.java
+++ eMobilePOSAndroid/src/main/java/com/android/emobilepos/settings/BixolonFragment.java
@@ -3,6 +3,7 @@ package com.android.emobilepos.settings;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.pm.ActivityInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -32,7 +33,7 @@ import drivers.EMSBixolonRD;
 
 public class BixolonFragment extends Fragment implements View.OnClickListener {
 
-    private EMSBixolonRD bixolon;
+    private EMSBixolonRD bixolonDevice;
     private TextView time;
     private TextView date;
     private TextView[] headersViews;
@@ -47,7 +48,7 @@ public class BixolonFragment extends Fragment implements View.OnClickListener {
     public BixolonFragment() {
         if (Global.mainPrinterManager != null && Global.mainPrinterManager.getCurrentDevice() != null
                 && Global.mainPrinterManager.getCurrentDevice() instanceof EMSBixolonRD) {
-            bixolon = (EMSBixolonRD) Global.mainPrinterManager.getCurrentDevice();
+            bixolonDevice = (EMSBixolonRD) Global.mainPrinterManager.getCurrentDevice();
         }
     }
 
@@ -64,7 +65,7 @@ public class BixolonFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        if (bixolon != null) {
+        if (bixolonDevice != null && getView() != null) {
             new InitTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
             getView().findViewById(R.id.sendDateTimebutton2).setOnClickListener(this);
             getView().findViewById(R.id.sendHeaderbutton2b).setOnClickListener(this);
@@ -73,7 +74,7 @@ public class BixolonFragment extends Fragment implements View.OnClickListener {
             getView().findViewById(R.id.sendPaymentMethodsbutton28).setOnClickListener(this);
         }
         Bixolon bixolon = BixolonDAO.getBixolon();
-        if (bixolon != null) {
+        if (bixolon != null && getView() != null) {
             ((EditText) getView().findViewById(R.id.bixolonructextView2)).setText(bixolon.getRuc());
             ((EditText) getView().findViewById(R.id.bixolonmerchantNametextView2)).setText(bixolon.getMerchantName());
 
@@ -129,6 +130,7 @@ public class BixolonFragment extends Fragment implements View.OnClickListener {
 
         @Override
         protected void onPreExecute() {
+            getActivity().setRequestedOrientation(Global.getScreenOrientation(getActivity()));
             dialog.setIndeterminate(true);
             dialog.setMessage(getString(R.string.processing));
             dialog.show();
@@ -139,16 +141,16 @@ public class BixolonFragment extends Fragment implements View.OnClickListener {
             Bixoloncommand command = params[0];
             switch (command) {
                 case SEND_DATE:
-                    return bixolon.sendDateTimeCommand(new Date());
+                    return bixolonDevice.sendDateTimeCommand(new Date());
                 case SEND_HEADER:
-                    return bixolon.sendHeaders(headers);
+                    return bixolonDevice.sendHeaders(headers);
                 case SEND_FOOTER:
-                    return bixolon.sendFooters(footers);
+                    return bixolonDevice.sendFooters(footers);
                 case SEND_TAXES:
                     TaxesHandler taxesHandler = new TaxesHandler(getActivity());
-                    return bixolon.sendTaxes(taxesHandler.getTaxes(true));
+                    return bixolonDevice.sendTaxes(taxesHandler.getTaxes(true));
                 case SEND_PAYMENT_METHODS:
-                    return bixolon.sendPaymentMethods(paymentMethods);
+                    return bixolonDevice.sendPaymentMethods(paymentMethods);
             }
             return null;
         }
@@ -161,18 +163,17 @@ public class BixolonFragment extends Fragment implements View.OnClickListener {
             } else {
                 Global.showPrompt(getActivity(), R.string.dlog_title_error, getString(R.string.bixolon_command_fail));
             }
+            getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         }
     }
 
     private class InitTask extends AsyncTask<Void, Void, Void> {
         ProgressDialog dialog = new ProgressDialog(getActivity());
         private Date printerDate;
-//        private double taxes[] = new double[5];
-//        private int typeTaxes[] = new int[5];
-
 
         @Override
         protected void onPreExecute() {
+            getActivity().setRequestedOrientation(Global.getScreenOrientation(getActivity()));
             dialog.setIndeterminate(true);
             dialog.setMessage(getString(R.string.loading));
             dialog.show();
@@ -183,18 +184,7 @@ public class BixolonFragment extends Fragment implements View.OnClickListener {
             try {
                 TaxesHandler taxesHandler = new TaxesHandler(getActivity());
                 taxes = taxesHandler.getTaxes(true);
-                printerDate = bixolon.getPrinterTFHKA().getS1PrinterData().getCurrentPrinterDateTime();
-//                this.taxes[0] = bixolon.getPrinterTFHKA().getS3PrinterData().getTax1();
-//                typeTaxes[0] = bixolon.getPrinterTFHKA().getS3PrinterData().getTypeTax1();
-//                this.taxes[1] = bixolon.getPrinterTFHKA().getS3PrinterData().getTax2();
-//                typeTaxes[1] = bixolon.getPrinterTFHKA().getS3PrinterData().getTypeTax2();
-//                this.taxes[2] = bixolon.getPrinterTFHKA().getS3PrinterData().getTax3();
-//                typeTaxes[2] = bixolon.getPrinterTFHKA().getS3PrinterData().getTypeTax3();
-//                this.taxes[3] = bixolon.getPrinterTFHKA().getS3PrinterData().getTax4();
-//                typeTaxes[3] = bixolon.getPrinterTFHKA().getS3PrinterData().getTypeTax4();
-//                this.taxes[4] = bixolon.getPrinterTFHKA().getS3PrinterData().getTax5();
-//                typeTaxes[4] = bixolon.getPrinterTFHKA().getS3PrinterData().getTypeTax5();
-
+                printerDate = bixolonDevice.getPrinterTFHKA().getS1PrinterData().getCurrentPrinterDateTime();
                 MemoTextHandler handler = new MemoTextHandler(getActivity());
                 headers = handler.getHeader();
                 footers = handler.getFooter();
@@ -211,95 +201,90 @@ public class BixolonFragment extends Fragment implements View.OnClickListener {
             footersViews = new TextView[3];
             taxesViews = new TextView[5];
             typeTaxesViews = new TextView[5];
-            LinearLayout payMethodContainer = (LinearLayout) getView().findViewById(R.id.bixolon_paymentMethodsContainer);
-            LayoutInflater layoutInflater = getActivity().getLayoutInflater();
-            for (PaymentMethod paymentMethod : paymentMethods) {
-                LinearLayout row = (LinearLayout) layoutInflater.inflate(R.layout.bixolon_two_cols_row_layout, null);
-                TextView col1 = (TextView) row.findViewById(R.id.bixoloncolumn1);
-                TextView col2 = (TextView) row.findViewById(R.id.bixoloncolumn2);
-                col1.setText(paymentMethod.getPaymentmethod_type());
-                col2.setText(paymentMethod.getPaymethod_name());
-                payMethodContainer.addView(row);
-            }
+            if (getView() != null && getView().findViewById(R.id.bixolon_paymentMethodsContainer) != null) {
+                LinearLayout payMethodContainer = (LinearLayout) getView().findViewById(R.id.bixolon_paymentMethodsContainer);
+                LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+                for (PaymentMethod paymentMethod : paymentMethods) {
+                    LinearLayout row = (LinearLayout) layoutInflater.inflate(R.layout.bixolon_two_cols_row_layout, null);
+                    TextView col1 = (TextView) row.findViewById(R.id.bixoloncolumn1);
+                    TextView col2 = (TextView) row.findViewById(R.id.bixoloncolumn2);
+                    col1.setText(paymentMethod.getPaymentmethod_type());
+                    col2.setText(paymentMethod.getPaymethod_name());
+                    payMethodContainer.addView(row);
+                }
 
-            date = (TextView) getView().findViewById(R.id.bixolondatetextView25);
-            headersViews[0] = (TextView) getView().findViewById(R.id.bixolonheader1textView25b);
-            headersViews[1] = (TextView) getView().findViewById(R.id.bixolonheader2textView25);
-            headersViews[2] = (TextView) getView().findViewById(R.id.bixolonheader3textView25);
+                date = (TextView) getView().findViewById(R.id.bixolondatetextView25);
+                headersViews[0] = (TextView) getView().findViewById(R.id.bixolonheader1textView25b);
+                headersViews[1] = (TextView) getView().findViewById(R.id.bixolonheader2textView25);
+                headersViews[2] = (TextView) getView().findViewById(R.id.bixolonheader3textView25);
 
-            footersViews[0] = (TextView) getView().findViewById(R.id.bixolonfooter1textView25c);
-            footersViews[1] = (TextView) getView().findViewById(R.id.bixolonfooter2textView25c);
-            footersViews[2] = (TextView) getView().findViewById(R.id.bixolonfooter3textView25c);
+                footersViews[0] = (TextView) getView().findViewById(R.id.bixolonfooter1textView25c);
+                footersViews[1] = (TextView) getView().findViewById(R.id.bixolonfooter2textView25c);
+                footersViews[2] = (TextView) getView().findViewById(R.id.bixolonfooter3textView25c);
 
-            taxesViews[0] = (TextView) getView().findViewById(R.id.bixolontaxes1textView25);
-            taxesViews[1] = (TextView) getView().findViewById(R.id.bixolontaxes2textView26);
-            taxesViews[2] = (TextView) getView().findViewById(R.id.bixolontaxes3textView27);
-            taxesViews[3] = (TextView) getView().findViewById(R.id.bixolontaxes4textView28);
-            taxesViews[4] = (TextView) getView().findViewById(R.id.bixolontaxes5textView29);
+                taxesViews[0] = (TextView) getView().findViewById(R.id.bixolontaxes1textView25);
+                taxesViews[1] = (TextView) getView().findViewById(R.id.bixolontaxes2textView26);
+                taxesViews[2] = (TextView) getView().findViewById(R.id.bixolontaxes3textView27);
+                taxesViews[3] = (TextView) getView().findViewById(R.id.bixolontaxes4textView28);
+                taxesViews[4] = (TextView) getView().findViewById(R.id.bixolontaxes5textView29);
 
-            typeTaxesViews[0] = (TextView) getView().findViewById(R.id.taxType1textView25);
-            typeTaxesViews[1] = (TextView) getView().findViewById(R.id.taxType2textView29);
-            typeTaxesViews[2] = (TextView) getView().findViewById(R.id.taxType3textView26);
-            typeTaxesViews[3] = (TextView) getView().findViewById(R.id.taxType4textView27);
-            typeTaxesViews[4] = (TextView) getView().findViewById(R.id.taxType5textView28);
+                typeTaxesViews[0] = (TextView) getView().findViewById(R.id.taxType1textView25);
+                typeTaxesViews[1] = (TextView) getView().findViewById(R.id.taxType2textView29);
+                typeTaxesViews[2] = (TextView) getView().findViewById(R.id.taxType3textView26);
+                typeTaxesViews[3] = (TextView) getView().findViewById(R.id.taxType4textView27);
+                typeTaxesViews[4] = (TextView) getView().findViewById(R.id.taxType5textView28);
 
-            int i = 0;
-            for (Tax tax : taxes) {
-                taxesViews[i].setText(String.format("%s %s%%", getString(R.string.text_tax),
-                        String.valueOf(tax.getTaxRate())));
-                typeTaxesViews[i].setText(String.format("%s %s", getString(R.string.taxtype),
-                        String.valueOf(tax.getTaxName())));
-                i++;
-                if (i >= taxesViews.length) {
-                    break;
+                int i = 0;
+                for (Tax tax : taxes) {
+                    taxesViews[i].setText(String.format("%s %s%%", getString(R.string.text_tax),
+                            String.valueOf(tax.getTaxRate())));
+                    typeTaxesViews[i].setText(String.format("%s %s", getString(R.string.taxtype),
+                            String.valueOf(tax.getTaxName())));
+                    i++;
+                    if (i >= taxesViews.length) {
+                        break;
+                    }
+                }
+                if (printerDate != null) {
+                    date.setText(DateUtils.getDateAsString(printerDate, DateUtils.DATE_MMM_dd_yyyy_h_mm_a));
+                } else {
+                    date.setText(DateUtils.getDateAsString(new Date(), DateUtils.DATE_MMM_dd_yyyy_h_mm_a));
+                }
+
+                if (headers[0] != null && !headers[0].isEmpty()) {
+                    headersViews[0].setText(headers[0]);
+                } else {
+                    headersViews[0].setVisibility(View.GONE);
+                }
+                if (headers[1] != null && !headers[1].isEmpty()) {
+                    headersViews[1].setText(headers[1]);
+                } else {
+                    headersViews[1].setVisibility(View.GONE);
+                }
+                if (headers[2] != null && !headers[2].isEmpty()) {
+                    headersViews[2].setText(headers[2]);
+                } else {
+                    headersViews[2].setVisibility(View.GONE);
+                }
+
+                if (footers[0] != null && !footers[0].isEmpty()) {
+                    footersViews[0].setText(footers[0]);
+                } else {
+                    footersViews[0].setVisibility(View.GONE);
+                }
+                if (footers[1] != null && !footers[1].isEmpty()) {
+                    footersViews[1].setText(footers[1]);
+                } else {
+                    footersViews[1].setVisibility(View.GONE);
+                }
+                if (footers[2] != null && !footers[2].isEmpty()) {
+                    footersViews[2].setText(footers[2]);
+                } else {
+                    footersViews[2].setVisibility(View.GONE);
                 }
             }
-
-//            for (int i = 0; i < 5; i++) {
-//                taxesViews[i].setText(String.format("%s %s", getString(R.string.text_tax),
-//                        String.valueOf(taxes[i])));
-//                typeTaxesViews[i].setText(String.format("%s %s", getString(R.string.taxtype),
-//                        String.valueOf(typeTaxes[i])));
-//            }
-
-            if (printerDate != null) {
-                date.setText(DateUtils.getDateAsString(printerDate, DateUtils.DATE_MMM_dd_yyyy_h_mm_a));
-            } else {
-                date.setText(DateUtils.getDateAsString(new Date(), DateUtils.DATE_MMM_dd_yyyy_h_mm_a));
-            }
-
-            if (headers[0] != null && !headers[0].isEmpty()) {
-                headersViews[0].setText(headers[0]);
-            } else {
-                headersViews[0].setVisibility(View.GONE);
-            }
-            if (headers[1] != null && !headers[1].isEmpty()) {
-                headersViews[1].setText(headers[1]);
-            } else {
-                headersViews[1].setVisibility(View.GONE);
-            }
-            if (headers[2] != null && !headers[2].isEmpty()) {
-                headersViews[2].setText(headers[2]);
-            } else {
-                headersViews[2].setVisibility(View.GONE);
-            }
-
-            if (footers[0] != null && !footers[0].isEmpty()) {
-                footersViews[0].setText(footers[0]);
-            } else {
-                footersViews[0].setVisibility(View.GONE);
-            }
-            if (footers[1] != null && !footers[1].isEmpty()) {
-                footersViews[1].setText(footers[1]);
-            } else {
-                footersViews[1].setVisibility(View.GONE);
-            }
-            if (footers[2] != null && !footers[2].isEmpty()) {
-                footersViews[2].setText(footers[2]);
-            } else {
-                footersViews[2].setVisibility(View.GONE);
-            }
             dialog.dismiss();
+            getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         }
     }
 }
