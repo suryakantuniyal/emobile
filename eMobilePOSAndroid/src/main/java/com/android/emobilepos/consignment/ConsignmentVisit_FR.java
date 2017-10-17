fefcd725536025921df6dabc22a356e899b4b64f
gpadro <gpadro@e-nablercorp.com>
2017-10-17 09:46:16 -0400

    fix consignment invoice product

--- eMobilePOSAndroid/src/main/java/com/android/emobilepos/consignment/ConsignmentVisit_FR.java
+++ eMobilePOSAndroid/src/main/java/com/android/emobilepos/consignment/ConsignmentVisit_FR.java
@@ -286,9 +286,9 @@ public class ConsignmentVisit_FR extends Fragment implements OnClickListener {
     }
 
     private void processOrder() {
-        List<DataTaxes> listOrderTaxes = global.order.getListOrderTaxes();
-        global.order = new Order(activity);
-        global.order.setListOrderTaxes(listOrderTaxes);
+//        List<DataTaxes> listOrderTaxes = global.order.getListOrderTaxes();
+//        global.order = new Order(activity);
+//        global.order.setListOrderTaxes(listOrderTaxes);
         TaxesHandler taxHandler = new TaxesHandler(activity);
         BigDecimal _order_total = BigDecimal.valueOf(ordTotal);
 
