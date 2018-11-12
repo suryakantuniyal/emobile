ccc03e9cb409633f5feea7a7ba39ff4becaad796
Luis Camayd <luis.camayd@e-nablercorp.com>
2018-11-12 17:34:03 -0400

    Create SoundPayments (PAX) response parser.

--- eMobilePOSAndroid/src/main/java/util/XmlUtils.java
+++ eMobilePOSAndroid/src/main/java/util/XmlUtils.java
@@ -1,5 +1,6 @@
 package util;
 
+import com.android.emobilepos.models.pax.SoundPaymentsResponse;
 import com.android.emobilepos.models.xml.EMSPayment;
 import com.crashlytics.android.Crashlytics;
 
@@ -56,6 +57,102 @@ public class XmlUtils {
         }
 
         return emsPayment;
+    }
+
+    public static SoundPaymentsResponse getSoundPaymentsResponse(String xml) {
+        SoundPaymentsResponse soundPaymentsResponse = new SoundPaymentsResponse();
+        try {
+            XmlPullParserFactory xmlFactoryObject = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = xmlFactoryObject.newPullParser();
+            parser.setInput(new StringReader(xml));
+            int event = parser.getEventType();
+            String name;
+            while (event != XmlPullParser.END_DOCUMENT) {
+                switch (event) {
+                    case XmlPullParser.START_TAG:
+                        name = parser.getName();
+                        if (name != null) {
+                            if (name.equalsIgnoreCase("epayStatusCode")) {
+                                soundPaymentsResponse.setEpayStatusCode(parser.nextText());
+                            } else if (name.equalsIgnoreCase("statusCode")) {
+                                soundPaymentsResponse.setStatusCode(parser.nextText());
+                            } else if (name.equalsIgnoreCase("statusMessage")) {
+                                soundPaymentsResponse.setStatusMessage(parser.nextText());
+                            } else if (name.equalsIgnoreCase("CreditCardTransID")) {
+                                soundPaymentsResponse.setCreditCardTransID(parser.nextText());
+                            } else if (name.equalsIgnoreCase("AuthorizationCode")) {
+                                soundPaymentsResponse.setAuthorizationCode(parser.nextText());
+                            } else if (name.equalsIgnoreCase("pay_receipt")) {
+                                soundPaymentsResponse.setPay_receipt(parser.nextText());
+                            } else if (name.equalsIgnoreCase("pay_refnum")) {
+                                soundPaymentsResponse.setPay_refnum(parser.nextText());
+                            } else if (name.equalsIgnoreCase("pay_maccount")) {
+                                soundPaymentsResponse.setPay_maccount(parser.nextText());
+                            } else if (name.equalsIgnoreCase("pay_groupcode")) {
+                                soundPaymentsResponse.setPay_groupcode(parser.nextText());
+                            } else if (name.equalsIgnoreCase("pay_stamp")) {
+                                soundPaymentsResponse.setPay_stamp(parser.nextText());
+                            } else if (name.equalsIgnoreCase("pay_resultcode")) {
+                                soundPaymentsResponse.setPay_resultcode(parser.nextText());
+                            } else if (name.equalsIgnoreCase("pay_resultmessage")) {
+                                soundPaymentsResponse.setPay_resultmessage(parser.nextText());
+                            } else if (name.equalsIgnoreCase("pay_expdate")) {
+                                soundPaymentsResponse.setPay_expdate(parser.nextText());
+                            } else if (name.equalsIgnoreCase("pay_result")) {
+                                soundPaymentsResponse.setPay_result(parser.nextText());
+                            } else if (name.equalsIgnoreCase("recordnumber")) {
+                                soundPaymentsResponse.setRecordnumber(parser.nextText());
+                            } else if (name.equalsIgnoreCase("AVSZip")) {
+                                soundPaymentsResponse.setAVSZip(parser.nextText());
+                            } else if (name.equalsIgnoreCase("CardSecurityCodeMatch")) {
+                                soundPaymentsResponse.setCardSecurityCodeMatch(parser.nextText());
+                            } else if (name.equalsIgnoreCase("CardBalance")) {
+                                soundPaymentsResponse.setCardBalance(parser.nextText());
+                            } else if (name.equalsIgnoreCase("AuthorizedAmount")) {
+                                soundPaymentsResponse.setAuthorizedAmount(parser.nextText());
+                            } else if (name.equalsIgnoreCase("CardType")) {
+                                soundPaymentsResponse.setCardType(parser.nextText());
+                            } else if (name.equalsIgnoreCase("CCLast4")) {
+                                soundPaymentsResponse.setCCLast4(parser.nextText());
+                            } else if (name.equalsIgnoreCase("CCexpDate")) {
+                                soundPaymentsResponse.setCCexpDate(parser.nextText());
+                            } else if (name.equalsIgnoreCase("CCName")) {
+                                soundPaymentsResponse.setCCName(parser.nextText());
+                            } else if (name.equalsIgnoreCase("AID")) {
+                                soundPaymentsResponse.setAID(parser.nextText());
+                            } else if (name.equalsIgnoreCase("APPLAB")) {
+                                soundPaymentsResponse.setAPPLAB(parser.nextText());
+                            } else if (name.equalsIgnoreCase("ATC")) {
+                                soundPaymentsResponse.setATC(parser.nextText());
+                            } else if (name.equalsIgnoreCase("CVM")) {
+                                soundPaymentsResponse.setCVM(parser.nextText());
+                            } else if (name.equalsIgnoreCase("CVMMSG")) {
+                                soundPaymentsResponse.setCVMMSG(parser.nextText());
+                            } else if (name.equalsIgnoreCase("IAD")) {
+                                soundPaymentsResponse.setIAD(parser.nextText());
+                            } else if (name.equalsIgnoreCase("AC")) {
+                                soundPaymentsResponse.setAC(parser.nextText());
+                            } else if (name.equalsIgnoreCase("TVR")) {
+                                soundPaymentsResponse.setTVR(parser.nextText());
+                            } else if (name.equalsIgnoreCase("EntryMode")) {
+                                soundPaymentsResponse.setEntryMode(parser.nextText());
+                            } else if (name.equalsIgnoreCase("EntryModeMsg")) {
+                                soundPaymentsResponse.setEntryModeMsg(parser.nextText());
+                            }
+                        }
+                        break;
+                }
+                event = parser.next();
+            }
+        } catch (XmlPullParserException e) {
+            Crashlytics.logException(e);
+        } catch (UnsupportedEncodingException e) {
+            Crashlytics.logException(e);
+        } catch (IOException e) {
+            Crashlytics.logException(e);
+        }
+
+        return soundPaymentsResponse;
     }
 
     public static String replaceAction(String xml, String newAction) {
